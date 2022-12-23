554c4402803be9440d090f2e393cd27848c111bf
Darren Haba <darrenhaba@live.com>
2022-12-22 20:27:38 -0600

    Adding tests

--- tests/test_privateprefs.py
+++ tests/test_privateprefs.py
@@ -1,7 +1,9 @@
+# noinspection PyProtectedMember
 import pytest
 import privateprefs
 
-from privateprefs.privateprefs import _save, _save_dict
+# noinspection PyProtectedMember
+import privateprefs.internal as internal
 
 test_key = "test key"
 test_value = "test value"
@@ -15,13 +17,13 @@ def clear_text_file():
         file.write("")
 
 
-@pytest.fixture(autouse = True)
+@pytest.fixture(autouse=True)
 def setup_and_teardown_for_stuff():
     # set up
     clear_text_file()
     yield
     # tear down
-    # clear_text_file()
+    clear_text_file()
 
 
 def _load_test_file_str():
@@ -35,13 +37,13 @@ def _save_dict_str_to_file(dict_str):
 
 
 def test_save():
-    _save(test_key, test_value)
+    internal._save(test_key, test_value)
     str_form_text_file = _load_test_file_str()
     assert str_form_text_file == test_dict_as_str
 
 
 def test_save_dict():
-    _save_dict(test_dict)
+    internal._save_dict(test_dict)
     str_form_text_file = _load_test_file_str()
     assert str_form_text_file == test_dict_as_str
 
@@ -81,3 +83,20 @@ def test_load_dict_with_only_key_in_dict_text_file():
 def test_load_dict_from_empty_file():
     _save_dict_str_to_file("")
     assert privateprefs.load_dict(test_key) == {}
+
+
+def test_clear():
+    internal._save(test_key, test_value)
+    privateprefs.clear()
+    assert privateprefs.load(test_key) is None
+
+
+def test_delete():
+    internal._save(test_key, test_value)
+    internal._save("other test key", "other test value")
+    print(privateprefs.load(test_key))
+    did_save_value = privateprefs.load(test_key) == test_value
+    privateprefs.delete(test_key)
+    did_delete_value = privateprefs.load(test_key) is None
+    did_not_delete_other_test_value = privateprefs.load("other test key") == "other test value"
+    assert all([did_save_value, did_delete_value, did_not_delete_other_test_value])
