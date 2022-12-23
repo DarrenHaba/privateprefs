c278b239493268b3838789ba857adc7baa75dd1b
Darren Haba <darrenhaba@live.com>
2022-12-22 20:28:09 -0600

    Adding cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,10 +1,45 @@
-# noinspection PyProtectedMember
-
-import privateprefs._cli as cli
-
-def test_save():
-    cli._save("Fee", "fi")
+import argparse
+import privateprefs.internal.cli as cli
 
 
-def test_load():
-    assert False
+# def test_save():
+#     cli._save("Fee", "fi")
+#
+#
+# def test_load():
+#     assert False
+
+
+def test_save(capsys):
+    with capsys.disabled():
+        cli.main(["save", "foo", "bar"])
+    cli.main(["load", "foo"])
+    captured = capsys.readouterr()
+    assert captured.out.__contains__("bar")
+
+
+def test_load(capsys):
+    with capsys.disabled():
+        cli.main(["save", "foo", "bar"])
+    cli.main(["load", "foo"])
+    captured = capsys.readouterr()
+    assert captured.out.__contains__("bar")
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', required=True)
+    args = parser.parse_args(argv)
+    print(f'Hello {args.name}')
+
+
+# if __name__ == '__main__':
+#     sys.exit(main())
+
+
+def test_main_even_simpler(capsys):
+    main(["--name", "Jürgen"])
+    captured = capsys.readouterr()
+    print(5555555)
+    print(captured)
+    assert captured.out == "Hello Jürgen\n"
