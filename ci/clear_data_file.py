87960d8e47e53ea2df30d346b978f4ab48a2afad
Darren Haba <darrenhaba@live.com>
2022-12-25 14:37:41 -0600

    Bugfix: CI - No module named 'privateprefs' (2nd attempt)

--- ci/clear_data_file.py
+++ ci/clear_data_file.py
@@ -1,4 +1,9 @@
-import privateprefs as prefs
+import pkg_resources
+
+path_to_test_file = pkg_resources.resource_filename(__name__, 'privateprefs/data/prefs.txt')
 
 if __name__ == "__main__":
-    prefs.clear()
+    # path workaround, but works for ci
+    path_without_ci_dic = path_to_test_file.replace("privateprefs\\ci\\", "privateprefs\\")
+    with open(path_without_ci_dic, "w") as file:
+        file.write("")
