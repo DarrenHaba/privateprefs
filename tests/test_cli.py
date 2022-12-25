a44057176c3bc8574d6b325b9db4f01e26556cdb
Darren Haba <darrenhaba@live.com>
2022-12-25 13:42:15 -0600

    Test new commands

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,37 +1,76 @@
-import argparse
+import pytest
+
 import privateprefs.cli as cli
+
+test_key = "test key"
+test_value = "test value"
+
+
+@pytest.fixture(autouse=True)
+def setup_and_teardown():
+    # set up
+    cli.main(["delete", "--all"])
+    yield
+    # tear down
+    cli.main(["delete", "--all"])
 
 
 def test_save(capsys):
     with capsys.disabled():
-        cli.main(["save", "foo", "bar"])
-    cli.main(["load", "foo"])
-    captured = capsys.readouterr()
-    assert captured.out.__contains__("bar")
+        cli.main(["save", test_key, test_value])
+    cli.main(["load", test_key])
+    capture = capsys.readouterr()
+    assert capture.out.__contains__(test_value)
 
 
 def test_load(capsys):
     with capsys.disabled():
-        cli.main(["save", "foo", "bar"])
-    cli.main(["load", "foo"])
-    captured = capsys.readouterr()
-    assert captured.out.__contains__("bar")
+        cli.main(["save", test_key, test_value])
+    cli.main(["load", test_key])
+    capture = capsys.readouterr()
+    assert capture.out.__contains__(test_value)
 
 
-# def main(argv=None):
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--name', required=True)
-#     args = parser.parse_args(argv)
-#     print(f'Hello {args.name}')
-#
-#
-# # if __name__ == '__main__':
-# #     sys.exit(main())
-#
-#
-# def test_main_even_simpler(capsys):
-#     main(["--name", "Jürgen"])
-#     captured = capsys.readouterr()
-#     print(5555555)
-#     print(captured)
-#     assert captured.out == "Hello Jürgen\n"
+def test_list(capsys):
+    with capsys.disabled():
+        cli.main(["save", test_key, test_value])
+    cli.main(["list"])
+    capture = capsys.readouterr()
+    contains_test_key = capture.out.__contains__(test_key)
+    contains_test_value = capture.out.__contains__(test_value)
+    assert all([contains_test_key, contains_test_value])
+
+
+def test_list_empty(capsys):
+    cli.main(["list"])
+    capture = capsys.readouterr()
+    displays_empty_list = capture.out.__contains__("list is empty: (no key-values saved yet)")
+    assert displays_empty_list
+
+
+def test_delete_all(capsys):
+    with capsys.disabled():
+        cli.main(["save", test_key, test_value])
+        cli.main(["save", "other key", "other value"])
+    cli.main(["delete", "--all"])
+    capture = capsys.readouterr()
+    all_key_value_deleted = capture.out.__contains__("all prefs deleted")
+    assert all_key_value_deleted
+
+
+def test_delete_key(capsys):
+    with capsys.disabled():
+        cli.main(["save", test_key, test_value])
+    cli.main(["delete", test_key])
+    capture = capsys.readouterr()
+    test_value_deleted = capture.out.__contains__(test_value)
+    assert test_value_deleted
+
+
+def test_delete__no_args_given_error(capsys):
+    try:
+        cli.main(["delete"])
+    except SystemExit:
+        assert True
+    else:
+        assert False
