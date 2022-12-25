ca564c26dbf775de26c0bb7aa6a32561ebc5c0d1
Darren Haba <darrenhaba@live.com>
2022-12-25 13:52:59 -0600

    Remove commented out code

--- privateprefs/__init__.py
+++ privateprefs/__init__.py
@@ -1,5 +1,4 @@
 # SPDX-FileCopyrightText: 2022-present Darren Haba <darrenhaba@live.com>
 #
 # SPDX-License-Identifier: MIT
-# from .privateprefs import *
 from .privateprefs import load, load_dict, delete, clear, path_to_test_file
