ee4a5493b07c95f2b75937add96bc7ef86782089
Darren Haba <darrenhaba@live.com>
2022-12-30 00:31:48 -0600

    Remove cli access from package

--- privateprefs/cli/__init__.py
+++ privateprefs/cli/__init__.py
@@ -1,4 +1,1 @@
-# SPDX-FileCopyrightText: 2022-present Darren Haba <darrenhaba@live.com>
-#
-# SPDX-License-Identifier: MIT
-from .cli import main
+
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 import pytest
 
-import privateprefs.cli as cli
+from privateprefs.cli.cli import main
 
 test_key = "test key"
 test_value = "test value"
@@ -9,32 +9,32 @@ test_value = "test value"
 @pytest.fixture(autouse=True)
 def setup_and_teardown():
     # set up
-    cli.main(["delete", "--all"])
+    main(["delete", "--all"])
     yield
     # tear down
-    cli.main(["delete", "--all"])
+    main(["delete", "--all"])
 
 
 def test_save(capsys):
     with capsys.disabled():
-        cli.main(["save", test_key, test_value])
-    cli.main(["load", test_key])
+        main(["save", test_key, test_value])
+    main(["load", test_key])
     capture = capsys.readouterr()
     assert capture.out.__contains__(test_value)
 
 
 def test_load(capsys):
     with capsys.disabled():
-        cli.main(["save", test_key, test_value])
-    cli.main(["load", test_key])
+        main(["save", test_key, test_value])
+    main(["load", test_key])
     capture = capsys.readouterr()
     assert capture.out.__contains__(test_value)
 
 
 def test_list(capsys):
     with capsys.disabled():
-        cli.main(["save", test_key, test_value])
-    cli.main(["list"])
+        main(["save", test_key, test_value])
+    main(["list"])
     capture = capsys.readouterr()
     contains_test_key = capture.out.__contains__(test_key)
     contains_test_value = capture.out.__contains__(test_value)
@@ -42,7 +42,7 @@ def test_list(capsys):
 
 
 def test_list_empty(capsys):
-    cli.main(["list"])
+    main(["list"])
     capture = capsys.readouterr()
     displays_empty_list = capture.out.__contains__("list is empty: (no key-value pairs saved yet)")
     assert displays_empty_list
@@ -50,9 +50,9 @@ def test_list_empty(capsys):
 
 def test_delete_all(capsys):
     with capsys.disabled():
-        cli.main(["save", test_key, test_value])
-        cli.main(["save", "other key", "other value"])
-    cli.main(["delete", "--all"])
+        main(["save", test_key, test_value])
+        main(["save", "other key", "other value"])
+    main(["delete", "--all"])
     capture = capsys.readouterr()
     all_key_value_deleted = capture.out.__contains__("all prefs deleted")
     assert all_key_value_deleted
@@ -60,8 +60,8 @@ def test_delete_all(capsys):
 
 def test_delete_key(capsys):
     with capsys.disabled():
-        cli.main(["save", test_key, test_value])
-    cli.main(["delete", test_key])
+        main(["save", test_key, test_value])
+    main(["delete", test_key])
     capture = capsys.readouterr()
     test_value_deleted = capture.out.__contains__(test_value)
     assert test_value_deleted
@@ -69,7 +69,7 @@ def test_delete_key(capsys):
 
 def test_delete__no_args_given_error(capsys):
     try:
-        cli.main(["delete"])
+        main(["delete"])
     except SystemExit:
         assert True
     else:
