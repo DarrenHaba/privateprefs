18b21a783e39b67fcbe1e01560458a017947afea
Darren Haba <darrenhaba@live.com>
2023-01-07 14:43:07 -0600

    Refactor name

--- privateprefs/__init__.py
+++ privateprefs/__init__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2022-present Darren Haba <darrenhaba@live.com>
 #
 # SPDX-License-Identifier: MIT
-from .privateprefs import load, load_dict, delete, delete_all
+from .privateprefs import load, load_keys, delete, delete_all
