55a312c3199fb277179b2fc9f515e4e82b100c52
Darren Haba <darrenhaba@live.com>
2023-01-12 01:53:47 -0600

    Add test

--- tests/test_database.py
+++ tests/test_database.py
@@ -66,3 +66,50 @@ def test__delete_all():
     _db.write(test_key2, test_value2)
     _db.delete_all()
     assert _db.read(test_key) is None
+
+
+def test__delete_data_file():
+    _db.write(test_key, test_value)
+    dose_file_exists_before_delete = _db.PATH_TO_DATA_FILE.exists()
+    _db._delete_data_file()
+    dose_file_exists_after_delete = _db.PATH_TO_DATA_FILE.exists()
+    assert dose_file_exists_before_delete is True
+    assert dose_file_exists_after_delete is False
+
+
+def test___delete_project_data_dir():
+    _db.write(test_key, test_value)
+    dose_dir_exists_before_delete = _db.PATH_TO_USER_DATA_PROJECT_DIR.exists()
+    _db._delete_data_file()
+    _db._delete_project_data_dir()
+    dose_dir_exists_after_delete = _db.PATH_TO_USER_DATA_PROJECT_DIR.exists()
+    assert dose_dir_exists_before_delete is True
+    assert dose_dir_exists_after_delete is False
+
+
+def test___delete_project_data_dir__mock_exists__true(mocker):
+    mocker.patch(
+        'privateprefs.core.database.pathlib.Path.exists',
+        return_value=True
+    )
+    _db.write(test_key, test_value)
+    _db._delete_data_file()
+    _db._delete_project_data_dir()
+    dose_dir_exists_after_delete = _db.PATH_TO_USER_DATA_PROJECT_DIR.exists()
+    print(dose_dir_exists_after_delete)
+    assert dose_dir_exists_after_delete is True
+
+
+def test___delete_project_data_dir__mock_exists__false(mocker):
+    mocker.patch(
+        'privateprefs.core.database.pathlib.Path.exists',
+        return_value=False
+    )
+    _db.write(test_key, test_value)
+    _db._delete_data_file()
+    _db._delete_project_data_dir()
+    dose_dir_exists_after_delete = _db.PATH_TO_USER_DATA_PROJECT_DIR.exists()
+    print(dose_dir_exists_after_delete)
+    assert dose_dir_exists_after_delete is False
+
+
