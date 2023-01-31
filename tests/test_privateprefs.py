13c94207b26dbf4c58db143f7d083c7b586bd1fd
Darren Haba <darrenhaba@live.com>
2023-01-31 01:16:58 -0600

    Update tests to use group names

--- tests/test_privateprefs.py
+++ tests/test_privateprefs.py
@@ -1,23 +1,41 @@
+import pathlib
+from platformdirs import user_data_dir
 import pytest
+
 import privateprefs.core.database as _db
 import privateprefs as prefs
 
 test_key = "test key"
-test_key2 = "test key2"
 test_value = "test value"
+test_group = "test group"
+
+test_key2 = "test key2"
 test_value2 = "test value2"
+test_group2 = "test group2"
 
 
 @pytest.fixture(autouse=True)
 def setup_and_teardown():
-    # set up
-    prefs.delete_all()
+    # -- set up --
+
+    # create a new path for testing
+    _db.PATH_TO_USER_DATA_PROJECT_DIR = pathlib.Path(user_data_dir(_db.PROJECT_NAME + "_unit_test", appauthor=False))
+    # create a new data file used for testing
+    _db.PATH_TO_DATA_FILE = _db.PATH_TO_USER_DATA_PROJECT_DIR / "data_unit_test.ini"
+
+    # delete last data file used for testing (if left over for some reason)
+    if _db.PATH_TO_DATA_FILE.exists():
+        _db.PATH_TO_DATA_FILE.unlink()
+
     yield
-    # tear down
-    prefs.delete_all()
+    # -- tear down --
+
+    # delete the data file used for testing
+    if _db.PATH_TO_DATA_FILE.exists():
+        _db.PATH_TO_DATA_FILE.unlink()
 
 
-def test_load():
+def test__load():
     _db.write(test_key, test_value)
     assert prefs.load(test_key) == test_value
 
@@ -26,43 +44,69 @@ def test__load__return_null_if_key_does_not_exist():
     assert prefs.load(test_key) is None
 
 
-def test__read_keys__as_dict():
-    _db.write(test_key, test_value)
-    _db.write(test_key2, test_value2)
-    key_values = prefs.load_keys()
+def test__load_keys__as_dict():
+    _db.write(test_key, test_value, test_group)
+    _db.write(test_key2, test_value2, test_group)
+    key_values = prefs.load_keys(test_group)
     assert key_values[test_key] == test_value and key_values[test_key2] == test_value2
 
 
-def test__read_keys__as_dict__filtered():
-    _db.write(test_key, test_value)
-    _db.write(test_key2, test_value2)
-    key_values = prefs.load_keys(keys=[test_key2])
-    assert key_values[test_key2] == test_value2 and len(key_values) == 1
-
-
-def test__read_keys__as_list():
-    _db.write(test_key, test_value)
-    _db.write(test_key2, test_value2)
-    key_values = prefs.load_keys(return_as_list=True)
-    assert key_values[0][1] == test_value and key_values[1][1] == test_value2
-
-
-def test__read_keys__as_list__filtered():
-    _db.write(test_key, test_value)
-    _db.write(test_key2, test_value2)
-    key_values = prefs.load_keys(keys=[test_key2], return_as_list=True)
-    assert key_values[0][1] == test_value2 and len(key_values) == 1
-
-
-def test__delete():
-    _db.write(test_key, test_value)
-    _db.write(test_key2, test_value2)
+def test__delete__no_group():
+    _db.write(test_key, test_value, None)
+    dose_data_file_contain_test_key = _db.read(test_key) == test_value
+    assert dose_data_file_contain_test_key is True
     prefs.delete(test_key)
-    assert _db.read(test_key) is None and _db.read(test_key2) == test_value2
+    dose_data_file_contain_test_key = _db.read(test_key) == test_value
+    assert dose_data_file_contain_test_key is False
+
+
+def test__delete__with_group():
+    _db.write(test_key, test_value, test_group)
+    dose_data_file_contain_test_key = _db.read(test_key, test_group) == test_value
+    assert dose_data_file_contain_test_key is True
+    prefs.delete(test_key, test_group)
+    dose_data_file_contain_test_key = _db.read(test_key, test_group) == test_value
+    assert dose_data_file_contain_test_key is False
 
 
 def test__delete_all():
     _db.write(test_key, test_value)
     _db.write(test_key2, test_value2)
-    prefs.delete_all()
+    prefs.delete_all("NO_GROUP")
     assert _db.read(test_key) is None
+
+
+def test__delete_all__no_group():
+    _db.write(test_key, test_value)
+    dose_data_file_contain_test_key = _db.read(test_key) == test_value
+    assert dose_data_file_contain_test_key is True
+
+    _db.write(test_key2, test_value2, test_group2)
+    dose_data_file_contain_test_key = _db.read(test_key2, test_group2) == test_value2
+    assert dose_data_file_contain_test_key is True
+
+    prefs.delete_all("NO_GROUP")
+
+    dose_data_file_contain_test_key = _db.read(test_key) == test_value
+    assert dose_data_file_contain_test_key is False
+
+    dose_data_file_contain_test_key = _db.read(test_key2, test_group2) == test_value2
+    assert dose_data_file_contain_test_key is True
+
+
+def test__delete_all__with_group():
+    _db.write(test_key, test_value, test_group)
+    dose_data_file_contain_test_key = _db.read(test_key, test_group) == test_value
+    assert dose_data_file_contain_test_key is True
+
+    _db.write(test_key2, test_value2, test_group2)
+    dose_data_file_contain_test_key = _db.read(test_key2, test_group2) == test_value2
+    assert dose_data_file_contain_test_key is True
+
+    prefs.delete_all(test_group)
+
+    dose_data_file_contain_test_key = _db.read(test_key, test_group) == test_value
+    assert dose_data_file_contain_test_key is False
+
+    dose_data_file_contain_test_key = _db.read(test_key2, test_group2) == test_value2
+    assert dose_data_file_contain_test_key is True
